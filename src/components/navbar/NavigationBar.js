1e8b1e13229f6b0c9d92a89d8e00b0c475a6fc20
Daniel-gd7 <daniel.galvez1997@gmail.com>
2022-06-17 17:58:21 -0400

    Update navbar

--- src/components/navbar/NavigationBar.js
+++ src/components/navbar/NavigationBar.js
@@ -1,22 +1,31 @@
 import {observadorLogin} from '../../helper/kcio'
 import { Navbar,Container,Nav,Button } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom';
+import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
 
 export default function NavigationBar(){
     const navigate = useNavigate()
     return( <>
-             <Navbar bg="dark" variant="dark">
-   <Container>
-   <Navbar.Brand href="#home">Datavoz</Navbar.Brand>
-     <Nav className="me-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-       <Nav.Link href="#features">Features</Nav.Link>
-       <Nav.Link href="#pricing">Pricing</Nav.Link>
-     </Nav>
-     <Button variant="warning" onClick={()=>navigate('/NuevoSup')} >Agregar nuevo supervisor</Button>
-
-
-    </Container>
+  
+  <Navbar collapseOnSelect expand='lg' bg="dark" variant="dark">
+    
+  <Container>
+  <Navbar.Brand href="#home">Datavoz</Navbar.Brand>
+      <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse>
+            
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+              </Nav>
+              <Button variant="warning" onClick={()=>navigate('/NuevoSup')} >Agregar nuevo supervisor</Button>
+                
+        
+        </Navbar.Collapse>
+          
+  </Container>
+    
    </Navbar>
     </>
 
