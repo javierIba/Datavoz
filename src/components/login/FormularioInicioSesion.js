44431f272eab2873dd671d82b2a1bc69536c5979
javierIba <javier.i.ibanez.a@gmail.com>
2022-06-17 19:18:23 -0400

    login como async

--- src/components/login/FormularioInicioSesion.js
+++ src/components/login/FormularioInicioSesion.js
@@ -1,4 +1,4 @@
-import { Form, Button } from 'react-bootstrap'
+import { Form, Button, Container, Card } from 'react-bootstrap'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 const FormularioInicioSesion = (props) => {
@@ -7,6 +7,7 @@ const FormularioInicioSesion = (props) => {
         pass: ''
     });
     const [message, setMessage] = useState('');
+    
     const navigate = useNavigate();
     const handleOnChange = (e) => {
         switch (e.target.name) {
@@ -22,47 +23,48 @@ const FormularioInicioSesion = (props) => {
     }
     
 
-    const login = () => {
-        fetch("http://localhost:4000/api/login", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(dataFormulario)
-        }).then(res => {
-            return { response: res.json(), statusCode: res.status }
-        }).then(res => {
-            const { response, statusCode } = res;
-            setMessage(response.message)
-            if (statusCode == 200) {
-                document.cookie = `token=${response.token}; path=/;samesite=strict`
-                navigate('/home')
-            }
+    const login = async () => {
+        let peticionLogin  = await fetch("http://localhost:4000/api/login", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(dataFormulario)
+            });
+        let response = await peticionLogin.json();
+        let statusCode = peticionLogin.status;
+        if(statusCode==200){
+            document.cookie = `token=${response.token}; path=/;samesite=strict`
+            navigate('/home') 
         }
-        )
+        setMessage(response.message)
     }
 
     return (
-        <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Container className='text-center p-5'>
+        <Card bg="light" border="warning" >
+        <Card.Header className="text-center">Iniciar Sesión</Card.Header>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
 
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="text" name="usuario" placeholder="Por favor ingrese su nombre de usuario" onChange={(e) => handleOnChange(e)} />
-                <Form.Text className="text-muted">
-                    {message}
-                </Form.Text>
-            </Form.Group>
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="text" name="usuario" placeholder="Por favor ingrese su nombre de usuario" onChange={(e) => handleOnChange(e)} />
+                    <Form.Text className="text-muted">
+                        {message}
+                    </Form.Text>
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Contraseña</Form.Label>
-                <Form.Control type="password" placeholder="Password" name="pass" onChange={(e) => handleOnChange(e)} />
-            </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Contraseña</Form.Label>
+                    <Form.Control type="password" placeholder="Password" name="pass" onChange={(e) => handleOnChange(e)} />
+                </Form.Group>
 
-            <Button variant="primary" type="button" onClick={login}>
-                Enviar
-            </Button>
-        </Form>
-
+                <Button variant="warning" type="button" onClick={login}>
+                    Enviar
+                </Button>
+            </Form>
+        </Card>    
+    </Container>
     )
 }
 
